76250d8c4b4c24ddae9d1e853dee818e407dc594
gnappuraz <gnappoms@gmail.com>
2018-01-11 22:40:21 +0100

    Add logout and username for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :characters
   resources :actions
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
 
   get 'home/index'
 
